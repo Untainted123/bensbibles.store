0440f6d1e8a3c6246ffff3af87add256d4f14a6d
Stephen Ingram <Ingram.StephenE@gmail.com>
2018-03-23 22:25:09 -0500

    change to cap B when appropiate

--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -5,7 +5,7 @@ module CustomHelpers
 
   def full_title(page_title=nil)
     page_title ||= ""
-    base_title = "bensbibles"
+    base_title = "Bensbibles"
     if page_title.empty?
       base_title
     else
